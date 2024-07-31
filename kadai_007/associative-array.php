3f23658fa5c23de2b91caa5c648db8669654472c
rie2smile <167359502+rie2smile@users.noreply.github.com>
2024-07-31 10:13:34 +0900

    課題007追加

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>PHP基礎編</title>
+ </head>
+ 
+ <body>
+     <p>
+         <?php
+        $personal_deta = ['name' => 'onion' , 'price' => '200' , 'weight' => '160'];
+        print_r($personal_deta);
+         ?>
+         </p>
+     </body>
+     
+     </html>
